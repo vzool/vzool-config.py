2986cb55f5af0aec835107ff1f5ffa190f973de6
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2024-10-07 09:10:28 +0300

    حذف الاصدارات الغير مدعومة من الاختبارات التلقائية

--- setup.py
+++ setup.py
@@ -10,9 +10,6 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
@@ -20,7 +17,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires=">=3.5",
+    python_requires=">=3.9",
     packages=find_packages(
         include=[
             'config',
